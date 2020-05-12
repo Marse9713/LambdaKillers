b06b90470d3a9af8ceaaeb567e60db9f91f4e818
Marse9713 <65126173+Marse9713@users.noreply.github.com>
2020-05-12 17:52:05 +0200

    Update and rename TempoHeap.java to HeapSelect.java

--- primo semestre - progetto algoritmi/codici algoritmi per tempi/HeapSelect.java
+++ primo semestre - progetto algoritmi/codici algoritmi per tempi/HeapSelect.java
@@ -9,7 +9,7 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class TempoHeap
+public class HeapSelect
 {
 
     public static ArrayList<nodo> h1 = new ArrayList<>(0);
